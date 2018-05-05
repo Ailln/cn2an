59ff616c687e9a5b2c926caff01d0e61c459530e
user1 <user1@dovolopor.com>
2018-05-05 11:30:01 +0800

    v0.0.2 setup.py add markdown description.

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 setup(
     name='cn2an',
-    version='0.0.1',
+    version='0.0.2',
     author='HaveTwoBrush',
     author_email='kinggreenhall@gmail.com',
     url='https://v2ai.cn',
-    description='Convert Chinese numerals and Arabic numerals.',
     packages=['cn2an'],
     install_requires=[],
+    description='Convert Chinese numerals and Arabic numerals.',
+    setup_requires=['setuptools-markdown'],
+    long_description_markdown_filename='README.md',
     entry_points={
         'console_scripts': [
             'cn2an=cn2an:cn2an',
