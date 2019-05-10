68186f139390791d57a7af262b91e8ff6985283a
wangxiaoqiange <wangxiaoqiangs@gmail.com>
2019-05-10 20:33:13 +0800

    修复 yaml.load 警告

--- cn2an/utils.py
+++ cn2an/utils.py
@@ -1,9 +1,13 @@
 import os
-
 import yaml
 
 
 def get_default_conf():
     with open(f"{os.path.dirname(__file__)}/config.yaml", "r", encoding="utf-8") as f_config:
-        config_data = yaml.load(f_config)
+        config_data = yaml.load(f_config, Loader=yaml.FullLoader)
     return config_data
+
+
+if __name__ == "__main__":
+
+    get_default_conf()
