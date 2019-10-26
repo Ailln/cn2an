6d47d049af5bbd9ff390bc6c25877b8a71fa015f
Ailln <kinggreenhall@gmail.com>
2019-10-26 17:49:31 +0800

    fix readme and local test.

--- local_test.sh
+++ local_test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-source /anaconda3/etc/profile.d/conda.sh
+source ~/anaconda3/etc/profile.d/conda.sh
 
-echo -e "\nTest for python 3.6.3...\n"
-conda activate py363
+echo -e "\nTest for python 3.6.9...\n"
+conda activate py369
 
 python3 -m pip install -r requirements.txt --ignore-installed
 
@@ -12,8 +12,8 @@ python3 -m cn2an.an2cn_test
 
 conda deactivate
 
-echo -e "\nTest for python 3.7.3...\n"
-conda activate py373
+echo -e "\nTest for python 3.7.4...\n"
+conda activate py374
 
 python3 -m pip install -r requirements.txt --ignore-installed
 
