d5f628e85ab3d81bd7c00316fa24b060452dbe9b
guoqchen <guqchen1001@icloud.com>
2020-11-21 14:28:45 +0800

    fix bug when run exe in pyinstaller

--- cn2an/utils.py
+++ cn2an/utils.py
@@ -2,12 +2,23 @@ import os
 import time
 import pathlib
 import logging
+import sys
 
 import yaml
 
 
+def get_base_path():
+    application_path = ''
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.dirname(sys.executable)
+    elif __file__:
+        application_path = os.path.dirname(__file__)
+    return application_path
+
+
 def get_default_conf() -> dict:
-    with open(f"{os.path.dirname(__file__)}/config.yaml", "r", encoding="utf-8") as f_config:
+    application_path = get_base_path()
+    with open(f"{application_path}/config.yaml", "r", encoding="utf-8") as f_config:
         config_data = yaml.load(f_config, Loader=yaml.FullLoader)
     return config_data
 
